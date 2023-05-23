0812500ce35c9eb5cd844e99cb0e132871464a8c
Bill Siever <bsiever@gmail.com>
2023-05-23 10:02:19 -0500

    Pre-release review cleanup.

--- blelog.ts
+++ blelog.ts
@@ -1,16 +1,9 @@
-
-
-
-
-// Same icon as datalogger block? (f0ce), but blue?  
-//  line-chart: f201? paper-plane: f1d8? share-square: f045
-
 /**
  * Bluetooth Access to Data Logger's Data
  */
 //% color=#0000FF 
 //% icon="\uf045"       
-//% block="Logger Bluetooth"
+//% block="Log Bluetooth"
 //% group="micro:bit (V2)"
 namespace blelog {
     /**
@@ -26,14 +19,4 @@ namespace blelog {
         // Per https://github.com/microsoft/pxt-microbit/issues/4292
         0;
     }
-
-    // //% blelog="dumpBLELog" block="dump ble log"
-    // //% shim=blelog::dumpBLELog
-    // //% group="micro:bit (V2)"
-    // export function dumpBLELog() : void {
-    //     // Per https://github.com/microsoft/pxt-microbit/issues/4292
-    //     0;
-    // }
-
-
 }
--- shims.d.ts
+++ shims.d.ts
@@ -2,16 +2,7 @@
 
 
     /**
-     * Bill Siever
-     * 2022-10-16 Initial Version
-     *
-     * Development environment specifics:
-     * Written in Microsoft PXT
-     *
-     * This code is released under the [MIT License](http://opensource.org/licenses/MIT).
-     * Please review the LICENSE.md file included with this example. If you have any questions 
-     * or concerns with licensing, please contact techsupport@sparkfun.com.
-     * Distributed as-is; no warranty is given.
+     * Bluetooth Access to Data Logger's Data
      */
 
 declare namespace blelog {
